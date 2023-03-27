3798d37b3e43b5b38389556b68fcd1bdc5b6cbc7
DaniloADamasceno <danilo.engauto@gmail.com>
2023-03-27 15:17:23 -0300

    :pencil2: Primeiro component

--- FRONT-END Angular/Crud-Angular/src/app/app-routing.module.ts
+++ FRONT-END Angular/Crud-Angular/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+// Rotas Globais --> Rotas são URLs que o usuário pode acessar
+const routes: Routes = [
+  { path: '', redirectTo: '/stores', pathMatch: 'full' },
+  { path: 'stores', loadChildren: () => import('./department/department.module').then(path => path.DepartmentModule) },
+];
+{}
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
