5249f16cade23241ce5504786ea05669420e3118
DaniloADamasceno <danilo.engauto@gmail.com>
2023-06-06 16:25:15 -0300

    👨‍💻 Atualização de HTML de Demonstração.

--- BACK-END AWS/BackEnd Produto/src/main/java/com/aws/backend/BackEndProdutoApplication.java
+++ BACK-END AWS/BackEnd Produto/src/main/java/com/aws/backend/BackEndProdutoApplication.java
@@ -13,8 +13,11 @@ public class BackEndProdutoApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("\n");
+        System.out.println("✅ ✅");
         System.out.println("✅✅ API REST de Produtos iniciada com sucesso! ✅✅ ");
-        System.out.println("\n");
+        System.out.println("Desenvolvido por: Danilo A. Damasceno");
+        System.out.println("LinkedIn: https://www.linkedin.com/in/daniloadamasceno/");
+        System.out.println("GitHub: https://github.com/DaniloADamasceno");
+        System.out.println("✅ ✅");
     }
 }
